b927d010b04adc225a3b9cef5e63e89305ff1732
augusto rickes da silveira <ag.augusto.rickes@gmail.com>
2025-12-11 03:11:40 -0300

    first commit

---
+++ src/test/java/com/itau/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
