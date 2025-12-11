3a09097d531a61405265c5e7ef63feccd9ef6b96
augusto rickes da silveira <ag.augusto.rickes@gmail.com>
2025-12-11 04:24:25 -0300

    feat: criacao dos DTO req de mov

---
+++ src/main/java/com/itau/api/dto/MovimentacaoRequestDTO.java
@@ -0,0 +1,16 @@
+package com.itau.api.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovimentacaoRequestDTO {
+    private BigDecimal valor;
+}
