42fa4aa729246b71e428ea2b569cf6c37ebed0d3
augusto rickes da silveira <ag.augusto.rickes@gmail.com>
2025-12-24 12:22:09 -0300

    feat: add handler for MethodArgumentNotValidException to return detailed validation errors

--- src/main/java/com/itau/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itau/api/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.itau.api.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -9,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import com.itau.api.exception.ResourceNotFoundException;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -34,6 +37,26 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleAllUncaughtException(Exception ex, WebRequest request) {
         // This is a catch-all for any other exceptions, returning a 500 Internal Server Error
         return buildErrorResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex){
+    Map<String,Object> body = new LinkedHashMap<>();
+    body.put("timestamp",LocalDateTime.now());
+    body.put("status", HttpStatus.BAD_REQUEST.value());
+    body.put("error","Validation Error");
+
+
+    Map<String, String> fieldErrors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(error->{
+        String fieldName = ((FieldError)error).getField();
+        String errorMessage = error.getDefaultMessage();
+        fieldErrors.put(fieldName,errorMessage);
+    });
+
+    body.put("messages", fieldErrors);
+
+    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<Object> buildErrorResponse(Exception ex, HttpStatus status) {
